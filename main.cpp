6b2ff3d0d74c8bf34f767ca056fb44f4efd2d2ee
Scory237 <Scory237@gmail.com>
2026-04-09 22:38:10 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,7 @@
 //App → (your TCP/IP stack) → TAP device → Linux kernel → Network so appearently it works like this
-fd = open("/dev/net/tap", O_RDWR);
+fd = open("/dev/net/tap", O_RDWR); //virtual ethernet interface
+struct eth_hdr {
+    unsigned char dmac[6];
+    unsigned char smac[6];
+    uint16_t ethertype;
+};
