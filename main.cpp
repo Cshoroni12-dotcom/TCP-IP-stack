5f14be69b3a4591079ba114f276bddca6738e673
Scory237 <Scory237@gmail.com>
2026-04-09 22:41:21 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,3 +5,14 @@ struct eth_hdr {
     unsigned char smac[6];
     uint16_t ethertype;
 };
+struct eth_hdr *hdr = (struct eth_hdr *) buf; // gives perm
+read(fd, buf)
+↓
+parse Ethernet
+↓
+switch(ethertype)
+    → ARP handler
+    → (later) IP handler //appearently i dont quiet understand but it is how firewall works?
+//oh i get it 
+// now we need "arp'
+//so the arp solves the problem that we know ip but dont know mac (not apple btw)
